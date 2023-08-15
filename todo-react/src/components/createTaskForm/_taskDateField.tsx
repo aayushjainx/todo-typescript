cf7d142088c7dc6e9ea14e1d03d52d8cc0347ebc
aayushjainx <jaayush014@gmail.com>
2023-08-15 17:13:20 +0530

    feat: add props to date field

--- todo-react/src/components/createTaskForm/_taskDateField.tsx
+++ todo-react/src/components/createTaskForm/_taskDateField.tsx
@@ -1,19 +1,33 @@
 import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import React, { FC, ReactElement, useState } from 'react';
+import React, { FC, ReactElement } from 'react';
+import { IDateField } from './interfaces/IDateField';
+import PropTypes from 'prop-types';
 
-export const TaskDateField: FC = (): ReactElement => {
-  const [date, setDate] = useState<Date | null>(null);
+export const TaskDateField: FC<IDateField> = (props): ReactElement => {
+  const {
+    value = new Date(),
+    onChange = (e) => console.log(e),
+    disabled = false,
+  } = props;
+
   return (
     <>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <DesktopDatePicker
           label="Task Date"
           format="dd/MM/yyyy"
-          value={date}
-          onChange={(newValue) => setDate(newValue)}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
         />
       </LocalizationProvider>
     </>
   );
+};
+
+TaskDateField.propTypes = {
+  value: PropTypes.instanceOf(Date),
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
 };
