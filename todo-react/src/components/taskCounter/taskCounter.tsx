8411de0c6672c279d43e18d5e7d4e238c87e9883
aayushjainx <jaayush014@gmail.com>
2023-08-15 23:22:24 +0530

    feat: set border color based upon status

--- todo-react/src/components/taskCounter/taskCounter.tsx
+++ todo-react/src/components/taskCounter/taskCounter.tsx
@@ -2,6 +2,7 @@ import { Avatar, Box, Typography } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 import { ITaskCounter } from './interfaces/ITaskCounter';
 import { Status } from '../createTaskForm/enums/Status';
+import { emitCorrectBorderColor } from './helpers/emitCorrentBorderColor';
 
 export const TaskCounter: FC<ITaskCounter> = (props): ReactElement => {
   const { count = 0, status = Status.completed } = props;
@@ -20,11 +21,11 @@ export const TaskCounter: FC<ITaskCounter> = (props): ReactElement => {
             width: '96px',
             height: '96px',
             marginBottom: '16px',
-            borderColor: 'warning.light',
+            borderColor: `${emitCorrectBorderColor(status)}`,
           }}
         >
           <Typography color="#ffffff" variant="h4">
-            10
+            {count}
           </Typography>
         </Avatar>
         <Typography
