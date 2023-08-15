f8d70b0c2e014c2b4b4915a7566c82f393ec747a
aayushjainx <jaayush014@gmail.com>
2023-08-16 00:27:03 +0530

    feat: create task component

--- todo-react/src/components/taskArea/taskArea.tsx
+++ todo-react/src/components/taskArea/taskArea.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactElement } from 'react';
 import { Box, Grid } from '@mui/material';
 import { format } from 'date-fns';
 import { TaskCounter } from '../taskCounter/taskCounter';
+import { Task } from '../task/task';
 
 export const TaskArea: FC = (): ReactElement => {
   return (
@@ -25,8 +26,9 @@ export const TaskArea: FC = (): ReactElement => {
           <TaskCounter />
         </Grid>
         <Grid item display="flex" flexDirection="column" xs={10} md={8}>
-          <Box>Task will come here</Box>
-          <Box>Task will come here</Box>
+          <Task />
+          <Task />
+          <Task />
         </Grid>
       </Grid>
     </Grid>
