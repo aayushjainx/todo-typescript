08a75ef78d925b601858e67f1fc5158f89d1aba9
aayushjainx <jaayush014@gmail.com>
2023-08-17 00:01:54 +0530

    feat: add other task columns

--- todo-api/src/tasks/task.entity.ts
+++ todo-api/src/tasks/task.entity.ts
@@ -1,7 +1,24 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Priority } from '../enums/priority';
+import { Status } from '../enums/Status';
 
 @Entity()
 export class Tasks {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'text' })
+  title: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  date: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'enum', enum: Priority, default: Priority.medium })
+  priority: Priority;
+
+  @Column({ type: 'enum', enum: Status, default: Status.todo })
+  status: Status;
 }
