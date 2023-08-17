3c1981bac2ab660f7e3dd3074023f9d5714a2889
aayushjainx <jaayush014@gmail.com>
2023-08-17 21:38:12 +0530

    feat: use task repository to fetch all tasks

--- todo-api/src/tasks/tasks.controller.ts
+++ todo-api/src/tasks/tasks.controller.ts
@@ -3,5 +3,15 @@ import { Tasks } from './task.entity';
 
 export class TaskController {
   constructor(private taskRepository = AppDataStore.getRepository(Tasks)) {}
-  public async getAll(): Promise<Tasks[]> {}
+  public async getAll(): Promise<Tasks[]> {
+    try {
+      let allTasks: Tasks[] = await this.taskRepository.find({
+        order: {
+          date: 'ASC',
+        },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
