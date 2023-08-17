3577c168d9a3f96f8cec49c52591e9fd040aa385
aayushjainx <jaayush014@gmail.com>
2023-08-17 23:52:57 +0530

    feat: move validation logic in controller

--- todo-api/src/tasks/tasks.controller.ts
+++ todo-api/src/tasks/tasks.controller.ts
@@ -2,6 +2,7 @@ import { instanceToPlain } from 'class-transformer';
 import { AppDataStore } from '../../index';
 import { Tasks } from './tasks.entity';
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 
 class TaskController {
   public async getAll(req: Request, res: Response): Promise<Response> {
@@ -16,6 +17,14 @@ class TaskController {
     } catch (_err) {
       return res.json({ message: 'Something went wrong' }).status(500);
     }
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    return res.status(200).json({ message: 'Task created successfully' });
   }
 }
 
--- todo-api/src/tasks/tasks.router.ts
+++ todo-api/src/tasks/tasks.router.ts
@@ -1,20 +1,9 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { taskController } from './tasks.controller';
 import { createValidator } from './tasks.validator';
-import { validationResult } from 'express-validator';
 
 export const taskRouter: Router = Router();
 
 taskRouter.get('/tasks', taskController.getAll);
 
-taskRouter.post(
-  '/tasks',
-  createValidator,
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    return res.status(200).json({ message: 'Task created successfully' });
-  },
-);
+taskRouter.post('/tasks', createValidator, taskController.create);
