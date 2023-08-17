bc3fc4233e6e57b654a6a1cf61d7b85b7e47cb58
aayushjainx <jaayush014@gmail.com>
2023-08-17 21:35:53 +0530

    feat: create typeorm repository

--- todo-api/src/tasks/tasks.controller.ts
+++ todo-api/src/tasks/tasks.controller.ts
@@ -1,5 +1,7 @@
+import { AppDataStore } from '../../index';
 import { Tasks } from './task.entity';
 
 export class TaskController {
+  constructor(private taskRepository = AppDataStore.getRepository(Tasks)) {}
   public async getAll(): Promise<Tasks[]> {}
 }
