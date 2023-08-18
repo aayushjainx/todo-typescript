91c163daa1d0356baef36b360451b2b9a31cdf03
aayushjainx <jaayush014@gmail.com>
2023-08-19 00:31:03 +0530

    feat: interface for get request

--- todo-react/src/components/taskArea/taskArea.tsx
+++ todo-react/src/components/taskArea/taskArea.tsx
@@ -5,10 +5,14 @@ import { TaskCounter } from '../taskCounter/taskCounter';
 import { Task } from '../task/task';
 import { useQuery } from '@tanstack/react-query';
 import { sendApiRequest } from '../../helpers/sendApiRequest';
+import { ITaskApi } from './interfaces/ITaskApi';
 
 export const TaskArea: FC = (): ReactElement => {
   const { error, isLoading, data, refetch } = useQuery('tasks', async () => {
-    return await sendApiRequest('http://localhost:3200/tasks', 'GET');
+    return await sendApiRequest<ITaskApi[]>(
+      'http://localhost:3200/tasks',
+      'GET',
+    );
   });
   return (
     <Grid item md={8} px={4}>
