1b92995c0ec87a9079e0bc17d95ecdf03a799bf4
aayushjainx <jaayush014@gmail.com>
2023-08-19 01:44:37 +0530

    feat: count tasks based on status helper function

---
+++ todo-react/src/components/taskArea/helpers/countTask.ts
@@ -0,0 +1,8 @@
+import { Status } from '../../createTaskForm/enums/Status';
+import { ITaskApi } from '../interfaces/ITaskApi';
+
+export const countTasks = (tasks: ITaskApi[], status: Status): number => {
+  if (!Array.isArray(tasks)) return 0;
+  const totalTasks = tasks.filter((task) => task.status === status);
+  return totalTasks.length;
+};
